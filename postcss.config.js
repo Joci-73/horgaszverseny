a1d3cf64dadc4ff3e2cbab76eb61aacfcbf94354
Joci-73 <horgaszathonlap@gmail.com>
2026-01-21 17:31:21 +0100

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+4. **Commit changes**
+
+---
+
+## 📄 **Fájl 6: `.gitignore`**
+
+1. **"Add file"** → **"Create new file"**
+2. Fájl neve: `.gitignore`
+3. Másold be:
+```
+node_modules
+.next
+out
+.env.local
