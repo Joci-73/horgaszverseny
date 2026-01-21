529de29186d9c413c269adf387e82c75f4cab303
Joci-73 <horgaszathonlap@gmail.com>
2026-01-21 18:14:17 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,19 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-```
-
-4. **Commit changes**
-
----
-
-## 📄 **Fájl 6: `.gitignore`**
-
-1. **"Add file"** → **"Create new file"**
-2. Fájl neve: `.gitignore`
-3. Másold be:
-```
-node_modules
-.next
-out
-.env.local
